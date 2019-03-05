0f60d9130aa9b34a0917c8194c961194772ef6fa
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 12:18:03 +0300

    adding image model

--- picapp/models.py
+++ picapp/models.py
@@ -14,3 +14,8 @@ class Location(models.Model):
     def __str__(self):
         return self.loc_name
 
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =50)
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)

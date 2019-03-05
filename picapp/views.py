7a7591226a20ec60acaf865d7acf1832a15acb65
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 08:39:29 +0300

    creating the syste app

---
+++ picapp/views.py
@@ -0,0 +1,44 @@
+import datetime as dt
+from django.shortcuts import render
+from django.http  import HttpResponse
+
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('welcome')
+
+def pics_today(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def pics_today(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>pics for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)

16b4064f7b5c88f4bc7cef88677739b41ccb21ff
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 16:22:13 +0300

    search urls

--- picapp/urls.py
+++ picapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
      url('^today/$',views.pics_today,name='picsToday')
+    url('^today/$',views.pics_today,name='picsToday'),
+    url(r'^search/', views.search_results, name='search_results')
 ]

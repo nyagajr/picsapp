65a9daa9d4e133e259819c0b4570650ddff29813
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 16:21:52 +0300

    test cases

--- picapp/tests.py
+++ picapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.park= Category(category_name = 'National park')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.park,Category))
+
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.locale= Category(loc_name = 'Madagascar')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.locale,Location))

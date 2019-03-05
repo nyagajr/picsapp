9688051c4b03e36c87cac2982e38e578b300afe4
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 09:04:00 +0300

    reconfiguring the installled apps

--- pics/settings.py
+++ pics/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'picapp.app.PicappConfig',
+    'picapp.apps.PicappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

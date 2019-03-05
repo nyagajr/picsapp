e42995563c17235c1f238d87c9dd4b2c813d2191
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 10:42:58 +0300

    calling the date function

--- picapp/views.py
+++ picapp/views.py
@@ -1,11 +1,10 @@
 import datetime as dt
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome')
 
 def pics_today(request):
     date = dt.date.today()

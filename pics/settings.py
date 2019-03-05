956bf98f8b201c9f4519453c9bbac922112cc9d9
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-05 12:18:37 +0300

    changing db to postgresql

--- pics/settings.py
+++ pics/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'picapp',
+        'USER': 'muchoki',
+    'PASSWORD':'123',
     }
 }
 

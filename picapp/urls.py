e3b080e0ded46b358f8a839de3e7e0215b312645
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-06 12:11:36 +0300

    url for views

--- picapp/urls.py
+++ picapp/urls.py
@@ -1,10 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import url
 from . import views
 
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-     url('^today/$',views.pics_today,name='picsToday')
-    url('^today/$',views.pics_today,name='picsToday'),
+    url('^$',views.pics_today,name='picsToday'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

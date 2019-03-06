1bd77827eeae53903c408ba3e580cec528d17f00
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-06 12:09:46 +0300

    adding image field

--- picapp/models.py
+++ picapp/models.py
@@ -19,6 +19,11 @@ class Image(models.Model):
     image_description = models.CharField(max_length =50)
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey(Category)
+    image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.image_name
+
     @classmethod
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
